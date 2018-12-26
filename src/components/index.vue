906855c0a14a4d6c43d58f107cec17af7b9020e3
mfkdgefhn <mfkdgefhn@163.com>
2018-12-26 11:56:53 +0800

    enter

---
+++ src/components/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <tab>
+      <tab-item @on-item-click="handler">精选</tab-item>
+      <tab-item @on-item-click="handler">美食</tab-item>
+      <tab-item @on-item-click="handler">电影</tab-item>
+      <tab-item @on-item-click="handler">酒店</tab-item>
+      <tab-item @on-item-click="handler">外卖</tab-item>
+    </tab>
+  </div>
+</template>
+
+<script>
+
+export default {
+}
+</script>
---
+++ src/vux/index.js
@@ -0,0 +1,9 @@
+import Vue from 'vue'
+
+import { Icon } from 'vux'
+
+Vue.component('icon', Icon)
+
+export default new Icon({
+
+})
